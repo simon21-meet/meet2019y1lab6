bc5828d5aa56cf4469ed14bd8627313ad1c5fb28
simon21-meet <simon21@meet.mit.edu>
2019-07-28 11:30:59 -0400

    Stopping here

---
+++ day6/lab6-fun1.py
@@ -0,0 +1,15 @@
+
+total=0
+for number in range(1, 10 + 1):
+    print(number)
+    total = total + number
+print(total)
+
+
+def add_numbers (x,y):
+    for number in range (x,y):
+        print (number)
+    tot= total+number
+    print(total)
+add_numbers(10,45)
+add_numbers(134,3345)
