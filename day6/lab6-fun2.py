773e77a717ceddec6d7bfa84a8e56565ca002000
simon21-meet <simon21@meet.mit.edu>
2019-07-29 12:54:35 -0400

    Stopping here

--- day6/lab6-fun2.py
+++ day6/lab6-fun2.py
@@ -9,11 +9,31 @@ turtle.goto(0,0)
 #turtle.onkey(up, "Up")
 #turtle.goto(0,0)
 #turtle.listen()
+def on_move():
+    x, y = turtle.pos()
+    if turtle.direction=="Up":
+        turtle.goto(x,y+100)
+    elif turtle.direction=="Down":
+        turtle.goto(x,y-100)
+    elif turtle.direction=="Left":
+        turtle.left(x+100,y)
+    else:
+        turtle.right(x-100,y)
 turtle.direction=None
 def up():
     turtle.direction="Up"
-    print("you pressed the up key")
+    on_move()
 turtle.onkey(up, "Up")
 turtle.listen()
-def on_move():
+ 
+def down():
+    turtle.direction="Down"
+    on-move
+turtle.mainloop()
+        
+    
+    
+        
+        
+    
     
