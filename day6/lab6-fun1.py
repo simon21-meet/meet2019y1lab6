57feb1babc152d0b99a4ebec754281dc69af4039
simon21-meet <simon21@meet.mit.edu>
2019-07-29 05:08:36 -0400

    Stopping here

--- day6/lab6-fun1.py
+++ day6/lab6-fun1.py
@@ -1,4 +1,4 @@
-
+#2
 total=0
 for number in range(1, 10 + 1):
     print(number)
@@ -6,10 +6,26 @@ for number in range(1, 10 + 1):
 print(total)
 
 
+#3
 def add_numbers (x,y):
     for number in range (x,y):
         print (number)
     tot= total+number
     print(total)
 add_numbers(10,45)
-add_numbers(134,3345)
+
+#4
+def jamie_1(x,y):
+    total=0
+    for jamie in range(x,y+1):
+        print(jamie)
+        total=total+jamie
+
+    return(total)
+
+x=jamie_1(333,777)
+print(x)
+squared_x = x**2
+print(squared_x)
+
+
